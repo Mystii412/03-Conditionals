c4d3ab53515a16091f244298b5dd0ecfb0e744c1
CMP_MiRoche <Mystii412@gmail.com>
2025-10-03 09:34:27 -0400

    Added bot to higher lower

--- higherlower.py
+++ higherlower.py
@@ -1,10 +1,12 @@
 import random
 
-
 def higher_lower():
     """Sets tries to 0, and asks the user for a number, if higher or lower, it will say so, until the user guesses the number. Counts how many tries it takes"""
+    global tries
     tries = 0
+    global answer
     answer = random.randint(0,101)
+    print(answer)
     while True:
         num = 0
         try:
@@ -15,7 +17,7 @@ def higher_lower():
 
         if num == answer:
             tries+=1
-            print(f'You win in {tries} tries.')
+            print(f'You guessed the number in {tries} tries.')
             print(f"The number was {answer}")
             break
         elif num > 100 or num < 0 or num == str:
@@ -33,10 +35,42 @@ def play_again():
     play = str(input('Would you like to play again? (y)es / (n)o: ')).lower().strip()
     return play
 
+def bot_guess():
+    """Makes the bot guess in the middle of the range, and splits the range in half based on whether it's higher or lower"""
+    global guesses
+    guesses = 0
+    r1 = 0
+    r2 = 100
+
+    while True:
+        guess = (r1 + r2)/2
+        print(guess)
+        if guess > answer:
+            guesses+=1
+            r2=guess
+            r2 = round(r2)
+        elif guess == answer:
+            guesses+=1
+            print(f'The bot guessed the number in {guesses} guesses.')
+            break
+        else:
+            r1 = guess
+            r1 = round(r1)
+
+def results(bot_attempts,human_attempts):
+    """Takes bot guesses and human guesses and compares them to see who wins"""
+    if bot_attempts > human_attempts:
+        diff = bot_attempts - human_attempts
+        print(f'You win by {diff} guesses.')
+    elif bot_attempts < human_attempts:
+        diff = human_attempts - bot_attempts
+        print(f"Bot Wins by {diff} guesses.")
 
 def main():
     while True:
         higher_lower()
+        bot_guess()
+        results(guesses,tries)
         play = play_again()
         if play == 'y':
             pass
@@ -44,6 +78,7 @@ def main():
             print('Have a good day.')
             break
     
+
 
 if __name__ == '__main__':
     main()
